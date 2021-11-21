af870c8b9f2c63287f163b96ac086a380da6bb46
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2021-11-21 19:50:54 +0200

    wip

--- Functions.php
+++ Functions.php
@@ -5,9 +5,6 @@ namespace EthersPHP\Keccak256;
 
 
 if (!function_exists('keccak256')) {
-    /**
-     * Sets the renderer implementation.
-     */
     function keccak256($data): string
     {
         return Keccak256::hash($data);
